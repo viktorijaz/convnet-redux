7fe1c6a8d7d5087bdd179f5c2a6765fada524ccc
viktorija <viktorija11@gmail.com>
2019-01-31 13:22:56 +0100

    Implement Database, API yet to be done with images.db

--- server.py
+++ server.py
@@ -8,17 +8,36 @@ import os
 import CIFAR10_CNN_Predict_Image
 import tensorflow
 from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
 
 app = Flask(__name__, )
+
 CORS(app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///images.db'
 db = SQLAlchemy(app)
+ma = Marshmallow(app)
 
 class FileContents(db.Model):
     id=db.Column(db.Integer, primary_key=True)
     name=db.Column(db.String(300))
     data = db.Column(db.LargeBinary)
+
+class FileContentsSchema(ma.Schema):
+    class Meta:
+        model = FileContents
+        #fields = ('id', 'name', 'data')
+
+file_schema = FileContentsSchema()
+files_schema = FileContentsSchema(many=True)
+
+# endpoint to show all images
+@app.route("/api/v1.0/images2", methods=["GET"])
+def get_images2():
+    all_images = FileContents.query.all()
+    result = files_schema.dump(all_images)
+    return jsonify(result.data)
+
 
 #@app.errorhandler(400)
 def not_found(error):
@@ -29,11 +48,36 @@ def not_found(error):
     return make_response(jsonify( { 'error': 'Not found' } ), 404)
 
 images = [
-    {  'id': 1, 'id_key': 1, 'title': 'No Scrubs', 'duration': '4:05', 'url': 'img\deer.png' },
-    { 'id': 2, 'id_key': 2, 'title': 'Macarena', 'duration': '2:30', 'url': 'img/dog.png' },
-    { 'id': 3, 'id_key': 3, 'title': 'All Star', 'duration': '3:15', 'url': 'img/truck.png' },
-    { 'id': 4, 'id_key': 4, 'title': 'I Want it That Way server', 'duration': '1:45', 'url': 'img/airplane.png' },
-    { 'id': 5, 'id_key': 5, 'title': 'I Want it That Way server 5', 'duration': '1:45', 'url': 'img/airplane.png' }
+    {  'id': 1, 'id_key': 1, 'title': 'Airplane', 'url': 'img/airplane.png' },
+    {  'id': 2, 'id_key': 2, 'title': 'Airplane', 'url': 'img/airplane-1.png' },
+    {  'id': 3, 'id_key': 3, 'title': 'Airplane', 'url': 'img/airplane-2.png' },
+    {  'id': 4, 'id_key': 4, 'title': 'Automobile', 'url': 'img/automobile.png' },
+    {  'id': 5, 'id_key': 5, 'title': 'Automobile', 'url': 'img/automobile-1.png' },
+    {  'id': 6, 'id_key': 6, 'title': 'Automobile', 'url': 'img/automobile-2.png' },
+    {  'id': 7, 'id_key': 7, 'title': 'Bird', 'url': 'img/bird.png' },
+    {  'id': 8, 'id_key': 8, 'title': 'Bird', 'url': 'img/bird-1.png' },
+    {  'id': 9, 'id_key': 9, 'title': 'Bird', 'url': 'img/bird-2.png' },
+    {  'id': 10, 'id_key': 10, 'title': 'Cat', 'url': 'img/cat.png' },
+    {  'id': 11, 'id_key': 11, 'title': 'Cat', 'url': 'img/cat-1.png' },
+    {  'id': 12, 'id_key': 12, 'title': 'Cat', 'url': 'img/cat-2.png' },
+    {  'id': 13, 'id_key': 13, 'title': 'Deer', 'url': 'img/deer.png' },
+    {  'id': 14, 'id_key': 14, 'title': 'Deer', 'url': 'img/deer-1.png' },
+    {  'id': 15, 'id_key': 15, 'title': 'Deer', 'url': 'img/deer-2.png' },
+    {  'id': 16, 'id_key': 16, 'title': 'Dog', 'url': 'img/dog.png' },
+    {  'id': 17, 'id_key': 17, 'title': 'Dog', 'url': 'img/dog-1.png' },
+    {  'id': 18, 'id_key': 18, 'title': 'Dog', 'url': 'img/dog-2.png' },
+    {  'id': 19, 'id_key': 19, 'title': 'Frog', 'url': 'img/frog.png' },
+    {  'id': 20, 'id_key': 20, 'title': 'Frog', 'url': 'img/frog-1.png' },
+    {  'id': 21, 'id_key': 21, 'title': 'Frog', 'url': 'img/frog-2.png' },
+    {  'id': 22, 'id_key': 22, 'title': 'Horse', 'url': 'img/horse.png' },
+    {  'id': 23, 'id_key': 23, 'title': 'Horse', 'url': 'img/horse-1.png' },
+    {  'id': 24, 'id_key': 24, 'title': 'Horse', 'url': 'img/horse-2.png' },
+    {  'id': 25, 'id_key': 25, 'title': 'Ship', 'url': 'img/ship.png' },
+    {  'id': 26, 'id_key': 26, 'title': 'Ship', 'url': 'img/ship-1.png' },
+    {  'id': 27, 'id_key': 27, 'title': 'Ship', 'url': 'img/ship-2.png' },
+    {  'id': 28, 'id_key': 28, 'title': 'Truck', 'url': 'img/truck.png' },
+    {  'id': 29, 'id_key': 29, 'title': 'Truck', 'url': 'img/truck-1.png' },
+    {  'id': 30, 'id_key': 30, 'title': 'Truck', 'url': 'img/truck-2.png' }
 ]
 
 def CNN_predict(path):
@@ -104,6 +148,7 @@ def handle():
    db.session.commit()
 
    return 'Saved ' + file.filename + ' to the database'
+
 
 @app.route('/api/v1.0/images', methods = ['GET'])
 def get_images():
